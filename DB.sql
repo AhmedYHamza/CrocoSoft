c28b54724c846826cc320989eaa3c2c55642875e
AhmedYHamza <ahmedyhamza@gmail.com>
2021-09-27 02:50:37 +0200

    database revised

--- DB.sql
+++ DB.sql
@@ -1,120 +1,93 @@
-CREATE TABLE document (
-  doc_id Int NOT NULL,
-  iS_from_outside Bool,
-  doc_status Text,
-  PRIMARY KEY (doc_id)
-);
-
-CREATE TABLE outside_document(
-  doc_id Int NOT NULL,
-  receipt_date Date,
-  receipt_number Text,
-  PRIMARY KEY (doc_id), 
-  FOREIGN KEY (doc_id) REFERENCES document(doc_id)
-);
-
 CREATE TABLE employee (
-  ssn Int NOT NULL,
-  ename Text,
+  ssn Int,
+  emp_name Text,
   job_title Text,
   email Text,
   PRIMARY KEY (ssn)
 );
 
+CREATE TABLE document (
+  doc_id Int,
+  is_from_outside Bool,
+  doc_status Text,
+  date_created Date,
+  date_modified Date,
+  PRIMARY KEY (doc_id)
+);
+
 CREATE TABLE draft (
-  doc_id Int NOT NULL,
-  draft_id Int NOT NULL,
-  PRIMARY KEY (doc_id,draft_id),
+  draft_id Int,
+  doc_id Int,
+  PRIMARY KEY (draft_id),
   FOREIGN KEY (doc_id) REFERENCES document(doc_id)
 );
 
 CREATE TABLE draft_copy (
-  doc_id Int NOT NULL,
-  draft_id Int NOT NULL,
-  copy_id Int NOT NULL,
-  PRIMARY KEY (doc_id,draft_id,copy_id),
-  FOREIGN KEY (doc_id,draft_id) REFERENCES draft(doc_id,draft_id)
+  copy_id Int,
+  draft_id Int,
+  PRIMARY KEY (copy_id),
+  FOREIGN KEY (draft_id) REFERENCES draft(draft_id)
+);
+
+CREATE TABLE outside_document (
+  doc_id Int,
+  receipt_date Date,
+  receipt_num Text,
+  PRIMARY KEY (doc_id),
+  FOREIGN KEY (doc_id) REFERENCES document(doc_id)
 );
 
 CREATE TABLE document_circulation (
-  circulation_id Int NOT NULL,
-  doc_id Int NOT NULL,
-  essn Int NOT NULL,
+  circulation_id Int,
+  source_id Int,
+  source_type Text,
+  emp_ssn Int,
+  modify_date Date,
   PRIMARY KEY (circulation_id),
-  FOREIGN KEY (doc_id) REFERENCES document(doc_id),
-  FOREIGN KEY (essn) REFERENCES employee(ssn)
+  FOREIGN KEY (emp_ssn) REFERENCES employee(ssn)
 );
 
-CREATE TABLE copy_circulation (
-  circulation_id Int NOT NULL,
-  doc_id Int NOT NULL,
-  draft_id Int NOT NULL,
-  copy_id Int NOT NULL,
-  essn Int NOT NULL,
-  PRIMARY KEY (circulation_id),
-  FOREIGN KEY (doc_id,draft_id,copy_id) REFERENCES draft_copy(doc_id,draft_id,copy_id)
-);
-
-CREATE TABLE draft_circulation (
-  circulation_id Int NOT NULL,
-  doc_id Int NOT NULL,
-  draft_id Int NOT NULL,
-  essn Int NOT NULL,
-  PRIMARY KEY (circulation_id),
-  FOREIGN KEY (doc_id,draft_id) REFERENCES draft(doc_id,draft_id),
-  FOREIGN KEY (essn) REFERENCES employee(ssn)
-);
-
-INSERT INTO document(doc_id,is_from_outside,doc_status)
-    VALUES (1,0,"Recieved"),(2,1,"Recieved"),(3,0,"Recieved"),(4,1,"Recieved"),(5,0,"Recieved");
+INSERT INTO document(doc_id,is_from_outside,doc_status,date_created,date_modified)
+    VALUES (1,0,"Recieved",CURDATE(),null),
+    (2,1,"Recieved",CURDATE(),null),
+    (3,0,"Recieved",CURDATE(),null),
+    (4,1,"Recieved",CURDATE(),null),
+    (5,0,"Recieved",CURDATE(),null);
     
 SELECT * 
 FROM Document;
 
-INSERT INTO outside_document(doc_id,receipt_date,receipt_number)
+INSERT INTO outside_document(doc_id,receipt_date,receipt_num)
     VALUES (2,'2020-5-4',255),(4,'2020-6-5',1222);
     
-INSERT INTO employee(ssn,ename,job_title,email)
+INSERT INTO employee(ssn,emp_name,job_title,email)
     VALUES (1,"ahmed","software engineer","ahmed@gmail.com"),
 			(2,"mohamed","backend engineer","mohamed@gmail.com"),
             (3,"mahmoud","QA engineer","mahmoud@gmail.com"),
             (4,"yasser","Product manager","yasser@gmail.com"),
             (5,"hamza","frontend engineer","hamza@gmail.com");
 
-INSERT INTO draft(doc_id,draft_id)
+INSERT INTO draft(draft_id,doc_id)
     VALUES (1,1),
-			(1,2),
+			(2,2),
             (3,1),
             (4,1),
-            (5,1);
+            (5,4);
             
-INSERT INTO draft_copy(doc_id,draft_id,copy_id)
-    VALUES (1,1,1),
-			(1,1,2),
-			(1,2,1),
-            (4,1,1),
-            (5,1,1);
+INSERT INTO draft_copy(copy_id,draft_id)
+    VALUES (1,2),
+			(2,1),
+			(3,1),
+            (4,5),
+            (5,3);
             
-INSERT INTO document_circulation(circulation_id,doc_id,essn)
-    VALUES (1,1,1),
-			(2,2,2),
-			(3,1,3),
-            (4,4,1),
-            (5,5,4);
+INSERT INTO document_circulation(circulation_id,source_id,source_type,emp_ssn,modify_date)
+    VALUES (1,1,"doc",1,CURDATE()),
+			(2,2,"drft",2,CURDATE()),
+			(3,1,"cpy",3,CURDATE()),
+            (4,4,"drft",1,CURDATE()),
+            (5,5,"doc",4,CURDATE());
             
-INSERT INTO draft_circulation(circulation_id,doc_id,draft_id,essn)
-    VALUES (1,1,1,1),
-			(2,1,2,2),
-			(3,4,1,3),
-            (4,3,1,1),
-            (5,5,1,4);
-            
-INSERT INTO copy_circulation(circulation_id,doc_id,draft_id,copy_id,essn)
-    VALUES (1,1,1,1,1),
-			(2,1,1,2,2),
-			(3,4,1,1,3),
-            (4,1,2,1,1),
-            (5,5,1,1,4);
 
 SELECT *
-FROM outside_document;
+FROM document_circulation;
