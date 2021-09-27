329baa1ddfcbbaefcaa3c7bc229f0a2b3695f5e2
AhmedYHamza <ahmedyhamza@gmail.com>
2021-09-27 05:29:59 +0200

    basic update

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ mysql = MySQL(app)
 
 api = Api(app)
 
+
 # Add queries
-
-
 class addDoc(Resource):
     def put(self):
         data = list(request.form.values())
@@ -163,6 +162,59 @@ class delEmp(Resource):
         return {}
 
 
+# update queries
+class updtDoc(Resource):
+    def put(self, doc_id):
+        data = list(request.form.values())
+        data.append(doc_id)
+        cursor = mysql.get_db().cursor()
+        cursor.execute('''
+        UPDATE document 
+        SET doc_id=%s,is_from_outside=%s,doc_status=%s,date_created=%s,date_modified=%s
+        WHERE document.doc_id=%s''', data)
+        mysql.get_db().commit()
+        return str(data)
+
+
+class updtDraft(Resource):
+    def put(self, draft_id):
+        data = list(request.form.values())
+        data.append(draft_id)
+        cursor = mysql.get_db().cursor()
+        cursor.execute('''
+        UPDATE draft
+        SET draft_id=%s,doc_id=%s
+        WHERE draft.draft_id=%s''', data)
+        mysql.get_db().commit()
+        return str(data)
+
+
+class updtCopy(Resource):
+    def put(self, copy_id):
+        data = list(request.form.values())
+        data.append(copy_id)
+        cursor = mysql.get_db().cursor()
+        cursor.execute('''
+        UPDATE draft_copy
+        SET copy_id=%s,draft_id=%s
+        WHERE draft_copy.copy_id=%s''', data)
+        mysql.get_db().commit()
+        return str(data)
+
+
+class updtEmp(Resource):
+    def put(self, ssn):
+        data = list(request.form.values())
+        data.append(ssn)
+        cursor = mysql.get_db().cursor()
+        cursor.execute('''
+        UPDATE employee
+        ssn=%s,emp_name=%s,job_title=%s,email=%s
+        WHERE employee.ssn=%s''', data)
+        mysql.get_db().commit()
+        return str(data)
+
+
 # Add
 api.add_resource(addDoc, '/add/doc')
 api.add_resource(addDraft, '/add/drft')
@@ -180,3 +232,9 @@ api.add_resource(delDoc, '/del/doc/<int:doc_id>')
 api.add_resource(delDraft, '/del/drft/<int:draft_id>')
 api.add_resource(delCopy, '/del/cpy/<int:copy_id>')
 api.add_resource(delEmp, '/del/emp/<int:ssn>')
+
+# Update
+api.add_resource(updtDoc, '/updt/doc/<int:doc_id>')
+api.add_resource(updtDraft, '/updt/drft/<int:draft_id>')
+api.add_resource(updtCopy, '/updt/cpy/<int:copy_id>')
+api.add_resource(updtEmp, '/updt/emp/<int:ssn>')
--- stub.py
+++ stub.py
@@ -2,11 +2,9 @@ import requests
 from datetime import date
 base = "http://127.0.0.1:5000/"
 
-# response = requests.put(base + "add/emp", {
-#    "ssn": "5",
-#    "emp_name": "hamza",
-#    "job_tite": "frontend engineer",
-#    "email": "hamza@gmail.com"
-# })
+response = requests.put(base + "updt/cpy/3", {
+    "copy_id": "3",
+    "draftid": "1"
+})
 #response = requests.get(base + "del/emp/5")
 print(response.json())
