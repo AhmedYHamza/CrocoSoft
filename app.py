8db9820055886f6c8b470e20726cdf43028e5472
AhmedYHamza <ahmedyhamza@gmail.com>
2021-09-27 04:14:27 +0200

    add document

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 from flaskext.mysql import MySQL
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = "root"
-app.config['MYSQL_DATABASE_PASSWORD'] = "dvddisc1A@"
+app.config['MYSQL_DATABASE_PASSWORD'] = "123456789"
 app.config['MYSQL_DATABASE_HOST'] = "127.0.0.1"
 app.config['MYSQL_DATABASE_DB'] = "crocosoft"
 app.config['MYSQL_DATABASE_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
+api = Api(app)
 
-@app.route('/')
-def index():
-    cursor = mysql.get_db().cursor()
 
-    cursor.execute('''
-    SELECT* 
-    FROM Document;''')
-    results = cursor.fetchall()
-    print(results)
-    return str(results)
+class addDoc(Resource):
+    def put(self):
+        data = list(request.form.values())
+        cursor = mysql.get_db().cursor()
+        cursor.execute('''
+        INSERT INTO document(doc_id,is_from_outside,doc_status,date_created,date_modified)
+        VALUES (%s,%s,%s,%s,%s)''', data)
+        mysql.get_db().commit()
+        return str(data)
+
+
+api.add_resource(addDoc, '/add/doc')
---
+++ stub.py
@@ -0,0 +1,12 @@
+import requests
+from datetime import date
+base = "http://127.0.0.1:5000/"
+
+response = requests.put(base + "add/doc", {
+    "doc_id": "6",
+    "is_from_outside": "1",
+    "doc_status": "recieved",
+    "date_created": "2021-09-27",
+    "date_modified": "2021-09-27"
+})
+print(response.json())
